fa50352cc8a2100616b7f17e010d8bbd49f4ffaa
Callum McLuskey <callummcluskey100@gmail.com>
2022-10-25 13:25:59 +0100

    removed old ts folder

--- templates/typescript/src/index.ts
+++
@@ -1,6 +0,0 @@
-import {DeviceController} from '@espruino-tools/device-controller'
-import './styles/app.scss'
-const device = new DeviceController();
-
-export const connect = () => device.connect()
-
